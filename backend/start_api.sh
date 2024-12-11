dce4754945be9e9b9e6bb8204c5551b069645910
munju <ahnmunju@gmail.com>
2024-12-11 17:30:23 +0900

    mk log folder code : .sh->.dockerfile

--- backend/start_api.sh
+++ backend/start_api.sh
@@ -1,17 +1,7 @@
 #! /bin/bash
 
-# apt update 
-# apt install -y python3 && apt install -y python3-pip
-# new_path = "export PATH\"\$PATH:/usr/bin\""
-# echo "$new_path" >> ~/.bashrc
-# source ~/.bashrc
 pip install pandas==2.2.3
 pip install fastapi==0.115
 pip install uvicorn==0.32
-
-# 로그 폴더 생성
-log_dir="logs"
-mkdir -p "$log_dir"
-
 echo "----- RUN model -----"
 python3 model_api.py --host 0.0.0.0 --port 8888
--- frontend/start_demo.sh
+++ frontend/start_demo.sh
@@ -1,15 +1,5 @@
 #! /bin/bash
 
-# apt update 
-# apt install -y python3 && apt install -y python3-pip
-# new_path = "export PATH\"\$PATH:/usr/bin\""
-# echo "$new_path" >> ~/.bashrc
-# source ~/.bashrc
 pip install streamlit==1.39.0
-
-# 로그 폴더 생성
-log_dir="logs"
-mkdir -p "$log_dir"
-
 echo "----- RUN demo -----"
 streamlit run demo.py backend 8888 --server.address 0.0.0.0 --server.port 8080
